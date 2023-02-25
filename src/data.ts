6fe067a3b8c3b67359b4dc394e0aeca554424c2e
Meqdam Qudah <memuq56@gmail.com>
2023-02-25 23:30:12 +0300

    add loading paragraph

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,11 @@
 import { product } from "./types/data"
 
 export const getData = (call:(data:product[])=>void)=>{
+    document.querySelector(".listLoading").classList.remove("hide")
     fetch("https://fakestoreapi.com/products").then((res)=>
         res.json()
     ).then((json)=>{
+        document.querySelector(".listLoading").classList.add("hide")
         call(json)
     })
 }
